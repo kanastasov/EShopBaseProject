f2fd8c26f2084064c6a9381afee3e0cc92d7082b
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-05-03 13:10:01 +0200

    Home Screen Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 
 const App =() => {
@@ -11,8 +12,8 @@ const App =() => {
      <Header />
      <main className='py-3'>
        <Container>
-         <h1>Welcome to My Shop</h1>
-         </Container>
+        <HomeScreen/>         
+     </Container>
 
      </main>
      <Footer/> 
