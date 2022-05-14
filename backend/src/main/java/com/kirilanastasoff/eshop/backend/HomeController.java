ed88e83d4cd905226bfc3fa11c27d616f6e64e42
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-05-14 13:33:36 +0200

    read products file from backedn

--- backend/src/main/java/com/kirilanastasoff/eshop/backend/HomeController.java
+++ backend/src/main/java/com/kirilanastasoff/eshop/backend/HomeController.java
@@ -1,5 +1,10 @@
 package com.kirilanastasoff.eshop.backend;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return "Hello from Spring Boot";
+        return "Api is running...";
     }
+
+
+
+    /**
+     * load the products from the file
+     * @return
+     */
+    @GetMapping("/api/products")
+    public String products()throws IOException {
+        File resource = new ClassPathResource("products.js").getFile();
+		String text = new String(Files.readAllBytes(resource.toPath()));
+        return text;
+    }
+
 }
