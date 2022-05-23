3e16ae1414dcf653ab2101337e1a254e40bd6894
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-05-23 13:59:18 +0200

    add the message and loader components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,38 +1,43 @@
-import React, { useEffect} from 'react'
-import { Row,Col } from 'react-bootstrap'
+import React,{ useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import Product from '../components/Product'
-import { listProducts } from '../actions/productActions'
+import {Row, Col} from 'react-bootstrap'
+import { listProducts } from './../actions/productActions';
+import Message from './../components/Message';
+import Loader from './../components/Loader';
+
 
 const HomeScreen = () => {
 
   const dispatch = useDispatch()
-
-
   const productList = useSelector(state => state.productList)
   const {loading, error, products} = productList
 
 
-  useEffect(() =>{
-    dispatch(listProducts())
-    
+     useEffect(() => {
+        dispatch(listProducts())
     }, [dispatch])
 
+  
 
   return (
     <>
-        <h1>Latest Products</h1>
-      {loading ? (<h2>Loading... <h2/>) : error ? (<h3>{error}</h3>) : 
-             <Row>
-            {products.map(product =>(
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product= {product} />
-                </Col>
-            ))}
-        </Row>}
- 
-        
-  
+        <h1>Latest Products </h1>
+        {loading 
+        ? (<Loader />) 
+        : error ? (
+          <Message variant='danger'>
+            {error}
+           </Message>
+        ): (
+        <Row>
+            {products.map(product =>  
+                <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}/>   
+          </Col>
+            )}
+        </Row>
+        )}
     </>
   )
 }
