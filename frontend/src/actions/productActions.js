a673bbb6aaadea4fdcc50bad721db27896eab7c9
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-05-23 10:59:46 +0200

    add the reducers and acations

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,16 @@
+import{PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} from '../constants/productConstatnts'
+import axios from 'axios';
+export const listProducts = () => async (dispatch) => {
+    try{
+        dispatch({type:PRODUCT_LIST_REQUEST})
+
+        const {data} = await axios.get('/api/products')
+
+        dispatch({
+            type: PRODUCT_LIST_SUCCESS,
+            payload: data
+        })
+    }catch(error){
+
+    }
+}
