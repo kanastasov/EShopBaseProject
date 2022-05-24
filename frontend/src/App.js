220af0685d32dfd5bd025842452490938ccf2a38
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-05-24 10:14:23 +0200

    qty select add to card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 
 function App() {
@@ -21,6 +22,8 @@ function App() {
 
              <Route path="/" element={<HomeScreen />} exact/>
              <Route path="/product/:id" element={<ProductScreen />} />
+             <Route path="/cart/:id?" element={<CartScreen />} />
+
 
             
           
